4489278a69dc67426d8c6a574f30ca7c78f09ff5
edotempus <edo@tempusmedia.hr>
2020-11-17 15:06:28 +0100

    navbar mobile

--- components/Header.vue
+++ components/Header.vue
@@ -1,5 +1,17 @@
 <template>
-  <div>header</div>
+  <header class="bg-white flex px-5 py-3">
+    <div class="w-1/6"></div>
+    <div class="w-4/6 flex justify-center">
+      <img src="/images/logo.svg" alt="Vegan Sailing Logo" />
+    </div>
+    <div class="w-1/6 flex justify-end align-middle">
+      <button type="button" class="focus:outline-none">
+        <svg class="h-6 w-6" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path stroke="#154752" stroke-width="2" d="M0 1h20M0 10h20M0 19h20" />
+        </svg>
+      </button>
+    </div>
+  </header>
 </template>
 
 <style></style>
