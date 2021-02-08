7bf114a83b6697ccbe6a5240e1841c957fff2cc5
edotempus <edo@tempusmedia.hr>
2021-02-08 20:01:44 +0100

    prije zadnje

--- components/BoatAndCabin.vue
+++ components/BoatAndCabin.vue
@@ -20,10 +20,73 @@
       <a
         class="cursor-pointer hover:opacity-80"
         target="_blank"
-        href="https://vegansailing.eu/mytour/theasys02/"
+        href="https://vegansailing.eu/mytour/theasys02/index.html"
         ><img src="/images/boat02.jpg"
       /></a>
     </div>
+
+    <client-only>
+      <carousel
+        :items="1"
+        :autoplay="true"
+        :navigation="true"
+        :dots="false"
+        class="relative pt-10 pb-10 sm:pb-beneath-booking sm:pt-16 bnc-carousel"
+        :margin="5"
+        :responsive="{ 0: { items: 1 }, 960: { items: 3 } }"
+      >
+        <template slot="prev"
+          ><span
+            class="absolute z-10 ml-5 cursor-pointer prev top-28 sm:top-32"
+          >
+            <!-- without <p> not showing arrow? -->
+
+            <img class="icon-height sm:mt-6" src="/nav/left-arrow.svg"
+          /></span>
+        </template>
+        <div>
+          <a
+            class="cursor-pointer hover:opacity-80"
+            target="_blank"
+            href="https://vegansailing.eu/mytour/theasys01/index.html"
+            ><img src="/images/boat01.jpg"
+          /></a>
+        </div>
+        <div>
+          <a
+            class="cursor-pointer hover:opacity-80"
+            target="_blank"
+            href="https://vegansailing.eu/mytour/theasys02/index.html"
+            ><img src="/images/theasys02.jpg"
+          /></a>
+        </div>
+        <div>
+          <a
+            class="cursor-pointer hover:opacity-80"
+            target="_blank"
+            href="https://vegansailing.eu/mytour/theasys03/index.html"
+            ><img src="/images/boat01.jpg"
+          /></a>
+        </div>
+        <div>
+          <a
+            class="cursor-pointer hover:opacity-80"
+            target="_blank"
+            href="https://vegansailing.eu/mytour/theasys04/index.html"
+            ><img src="/images/boat02.jpg"
+          /></a>
+        </div>
+
+        <template slot="next"
+          ><span
+            class="absolute right-0 z-10 mr-5 cursor-pointer next top-28 sm:top-32"
+            ><img
+              class="icon-height sm:mt-6"
+              src="/nav/right-arrow.svg"
+              alt="next" /></span
+        ></template>
+      </carousel>
+    </client-only>
   </div>
 </template>
 
