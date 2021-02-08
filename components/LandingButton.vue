a39dd6dad2f5ab341e399b4ba21b8a998feb3ceb
edotempus <edo@tempusmedia.hr>
2021-02-08 15:09:49 +0100

    doradio bac hover

--- components/LandingButton.vue
+++ components/LandingButton.vue
@@ -4,13 +4,13 @@
 
     <nuxt-link :to="route" class="xs:hidden"
       ><div
-        class="landing-btn h-24 sm:h-40 group"
+        class="h-24 landing-btn sm:h-40 group"
         :class="
           variantState ? 'landing-btn--primary' : 'landing-btn--secondary'
         "
       >
-        <p class="font-poppins font-medium text-lg sm:text-4xl">{{ title }}</p>
-        <p class="font-poppins font-light italic text-lg sm:text-4xl">
+        <p class="text-lg font-medium font-poppins sm:text-4xl">{{ title }}</p>
+        <p class="text-lg italic font-light font-poppins sm:text-4xl">
           {{ subtitle }}
         </p>
       </div></nuxt-link
@@ -20,23 +20,23 @@
 
     <nuxt-link :to="route" class="hidden xs:block"
       ><div
-        class="landing-btn h-24 sm:h-40 hover:pt-10 hover:h-64 sm:hover:h-landing sm:hover:pt-8 my-transition"
+        class="h-24 landing-btn sm:h-40 hover:pt-10 hover:h-64 sm:hover:h-landing sm:hover:pt-8 my-transition"
         :class="
           variantState ? 'landing-btn--primary' : 'landing-btn--secondary'
         "
       >
         <p
-          class="font-poppins font-medium text-lg sm:text-4xl sm:mt-6 sm:leading-landing my-title"
+          class="text-lg font-medium font-poppins sm:text-4xl sm:mt-6 sm:leading-landing my-title"
         >
           {{ title }}
         </p>
         <p
-          class="font-poppins font-light italic text-lg sm:text-4xl sm:leading-landing"
+          class="text-lg italic font-light font-poppins sm:text-4xl sm:leading-landing"
         >
           {{ subtitle }}
         </p>
         <p
-          class="font-poppins content-text text-center max-w-landing sm:text-xl font-light leading-snug pt-2 sm:pt-6"
+          class="pt-2 font-light leading-snug text-center font-poppins content-text max-w-landing sm:text-xl sm:pt-6"
         >
           {{ content }}
         </p>
@@ -46,7 +46,7 @@
           :class="
             variantState ? 'btn--primary-landing' : 'btn--secondary-landing'
           "
-          class="btn bottom-btn xs:block mt-5 mb-5 sm:mt-12 sm:mb-4"
+          class="mt-5 mb-5 btn bottom-btn xs:block sm:mt-12 sm:mb-4"
         >
           {{ $t('components.vegebtn.see_more') }}</VegeBtn
         >
--- components/mapdates/MapDateItem.vue
+++ components/mapdates/MapDateItem.vue
@@ -1,20 +1,24 @@
 <template>
   <div
-    class="h-20 max-w-xl mx-5 my-1 bg-white border bg-opacity-80 content xs:w-88"
+    class="relative h-20 max-w-xl mx-5 my-1 bg-white border bg-opacity-80 content xs:w-88 group"
   >
-    <a :href="route">
-      <div
-        class="flex flex-col justify-center h-full pl-8 border-l-4 border-vegan-orange"
-      >
-        <div class="text-xl font-medium sm:text-2xl section-title">
-          {{ name
-          }}<span class="text-xl font-normal sm:text-2xl section-title">{{
-            region
-          }}</span>
+    <div class="w-0 h-20 group-hover:bg-vegan-orange stretcher">
+      <a :href="route">
+        <div
+          class="absolute z-20 flex flex-col justify-center h-full pl-8 border-l-4 border-vegan-orange"
+        >
+          <div class="text-xl font-medium sm:text-2xl section-title">
+            {{ name
+            }}<span class="text-xl font-normal sm:text-2xl section-title">{{
+              region
+            }}</span>
+          </div>
+          <div class="font-medium">
+            {{ date }}
+          </div>
         </div>
-        <div class="font-medium">{{ date }}</div>
-      </div>
-    </a>
+      </a>
+    </div>
   </div>
 </template>
 <script>
@@ -22,3 +26,11 @@ export default {
   props: ['name', 'date', 'region', 'route'],
 }
 </script>
+<style scoped>
+.group:hover .stretcher {
+  width: 100%;
+}
+.stretcher {
+  transition: 1s all ease-in-out;
+}
+</style>
