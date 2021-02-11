ec3755a36d0173f3fd1c6f05202584f6e7a52511
edotempus <edo@tempusmedia.hr>
2021-02-11 17:00:26 +0100

    zadnja

--- components/Preloader.vue
+++ components/Preloader.vue
@@ -2,15 +2,22 @@
   <div
     class="absolute z-50 justify-center w-full h-screen bg-white align-center"
   >
-    <img src="/images/carrot-loader.gif" class="mx-auto" />
-    <!-- <div class="relative top-1/3 loader"></div> -->
+    
+      <img src="/images/carrot-loader.gif" class="mx-auto"/>
+      <!-- <div class="relative top-1/3 loader"></div> -->
+      <div class="pl-5 mx-auto subtext "><p class="text-xs opacity-75 text-vegan-blue text-light font-poppins">LOADING...</p></div>
+    </div>
   </div>
 </template>
 <style scoped>
-img {
+img, .subtext {
   height: 100px !important;
   max-width: 143px !important;
   position: relative;
   top: 40%;
+}
+
+.subtext > p {
+  margin-left: 24px !important;
 }
 </style>
--- components/mapdates/MapDates.vue
+++ components/mapdates/MapDates.vue
@@ -120,7 +120,7 @@
               xlink:href="/images/bgmap02.jpg"
               width="100"
               height="100"
-              viewBox="0 0 100 100"
+              viewBox="0 0 500 500"
               preserveAspectRatio="none"
             ></image>
           </pattern>
